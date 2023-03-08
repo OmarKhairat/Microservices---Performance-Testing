f557f7dfdded12cbf33cc807c865f9faa2d5be43
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-03-08 16:24:46 +0200

    Modified rating class

--- ratings-data-service/src/main/java/com/example/ratingsservice/models/Rating.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/models/Rating.java
@@ -1,24 +1,20 @@
 package com.example.ratingsservice.models;
 
-//import jakarta.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Table;
-
-import javax.persistence.Entity;
-
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode
-@Setter
 @Getter
-@Entity
-@Table(appliesTo = "")
+@Setter
 @NoArgsConstructor
 public class Rating {
-    @javax.persistence.Id
-    private Long userId;
     private String movieId;
     private int rating;
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
 }
