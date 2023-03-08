e8ee857230e12f98e01ee335920627c504e4d6e1
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2023-03-08 15:35:17 +0200

    .

--- ratings-data-service/src/main/java/com/example/ratingsservice/repositories/UserRatingRepo.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/repositories/UserRatingRepo.java
@@ -1,15 +1,12 @@
 package com.example.ratingsservice.repositories;
 
 import com.example.ratingsservice.models.Rating;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRatingRepo extends CrudRepository<Rating, String> {
+public interface UserRatingRepo extends JPARepository<Rating, String> {
 
     @Query("SELECT R.movieId, R.rating FROM Rating R WHERE R.userId = :userId")
     List<Rating> findByUserId(@Param("userId") String userId);
