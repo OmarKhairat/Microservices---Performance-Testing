cf02a54b649c5aa0be8c24ed8b3c2ed0fe3e559d
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-03-08 16:25:39 +0200

    Separated the controller and the service.

--- ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
--- ratings-data-service/src/main/java/com/example/ratingsservice/services/RatingsService.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/services/RatingsService.java
@@ -1,20 +1,43 @@
 package com.example.ratingsservice.services;
 
 import com.example.ratingsservice.models.Rating;
-import com.example.ratingsservice.repositories.UserRatingRepo;
+import com.example.ratingsservice.models.RatingEntity;
+import com.example.ratingsservice.repositories.RatingRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RatingsService {
-    private final UserRatingRepo userRatingRepo;
+    private final RatingRepo ratingRepo;
 
-    public RatingsService(UserRatingRepo userRatingRepo) {
-        this.userRatingRepo = userRatingRepo;
+    public RatingsService(RatingRepo ratingRepo) {
+        this.ratingRepo = ratingRepo;
+    }
+
+    private static Rating generateRating(RatingEntity source) {
+        try {
+            ObjectMapper om = new ObjectMapper();
+            Rating target = new Rating();
+
+            String jsonstr = om.writeValueAsString(source);
+            om.readerForUpdating(target).readValue(jsonstr);
+
+            return target;
+        } catch (Exception ignored) {
+            return null;
+        } 
     }
 
     public List<Rating> getRatings(String userId) {
-        return userRatingRepo.findByUserId(userId);
+        List<Rating> ratings = new ArrayList<>();
+        
+        for (RatingEntity entity : ratingRepo.findByUserId(userId)) {
+            ratings.add(generateRating(entity));
+        }
+        
+        return ratings;
     }
 }
