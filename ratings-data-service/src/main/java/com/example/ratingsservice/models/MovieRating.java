cc5e8cf7170a542935f93f479668ccace53cdb45
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2023-03-11 14:57:16 +0200

    Update MovieRating.java

--- ratings-data-service/src/main/java/com/example/ratingsservice/models/MovieRating.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/models/MovieRating.java
@@ -1,23 +1,6 @@
 package com.example.ratingsservice.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
-@Setter
-@NoArgsConstructor
-@Getter
-@Data
 public class MovieRating {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String movieId;
     private int rating;
